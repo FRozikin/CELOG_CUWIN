29b6efe09bb0ea043bd98b39bd2012389f896bb0
Fahrur Rozikin <113170669+FRozikin@users.noreply.github.com>
2023-03-29 00:40:25 +0700

    Add files via upload

---
+++ CELOGDLL/Class1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.IO
+
+Public Class CELog
+
+    Public Shared Function CekDir() As String
+        Dim Dr As String = Directory.GetDirectoryRoot(Directory.GetCurrentDirectory)
+        Dim Y As String = (Today.Year).ToString
+        Dim M As String = Format(Today.Month, "00")
+
+        Dim cDir As String = Dr & Y
+        Dim cDirM As String = Dr & Y & "\" & M
+        If Directory.Exists(cDir) Then
+            'MsgBox(cDir & " : Direktori Exist")
+            Directory.SetCurrentDirectory(cDir)
+            If Directory.Exists(cDirM) Then
+                'MsgBox(cDirM & " : Direktori Exist")
+                Directory.SetCurrentDirectory(cDirM)
+            Else
+                Directory.CreateDirectory(cDirM)
+                Directory.SetCurrentDirectory(cDirM)
+            End If
+        Else
+            'MsgBox(cDir & " : Direktori not Exist")
+            Directory.CreateDirectory(cDir)
+            Directory.SetCurrentDirectory(cDir)
+            If Directory.Exists(cDirM) Then
+                'MsgBox(cDirM & " : Direktori Exist")
+                Directory.SetCurrentDirectory(cDirM)
+            Else
+                Directory.CreateDirectory(cDirM)
+                Directory.SetCurrentDirectory(cDirM)
+            End If
+        End If
+        CekDir = Directory.GetCurrentDirectory
+    End Function
+
+    Public Shared Sub WriteLog(ByVal WL As String, ByVal HJ As String)
+        Dim filePath As String = String.Format("{0}.csv", DateTime.Today.ToString("yyyy-MM-dd"))
+        Dim T As String = String.Format(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"))
+        T = T & ", " & WL & ", " & HJ
+        Dim CDir As String = CekDir() & "\" & filePath
+        '        Dim fileExist As Boolean = File.Exists(filePath)
+        Dim fileExist As Boolean = File.Exists(CDir)
+
+        Using writer As New StreamWriter(CDir, True)
+            Console.WriteLine(CDir)
+            If Not fileExist Then
+                writer.WriteLine("Date_Time, WaterLevel, RainFall")
+            End If
+            writer.WriteLine(T)
+
+        End Using
+
+    End Sub
+
+End Class
---
+++ CELOGDLL/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CELOGDLL")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft Corporation")> 
+<Assembly: AssemblyProduct("CELOGDLL")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2023")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: CLSCompliant(True)> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e509a5d3-7ba9-4065-b23f-052d1be722b8")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+<Assembly: AssemblyVersion("1.0.*")> 
